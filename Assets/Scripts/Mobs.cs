e8bd9169ab32a01eb4b6d8e1ecc1379ef233ea41
dwilsonaieyr1 <JittNA@aol.com>
2016-04-12 14:00:55 -0500

    Commit
    Commit

---
+++ Assets/Scripts/Mobs.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mobs : MonoBehaviour {
+
+    public GameObject Mob;
+    public Transform LBorder;
+    public Transform RBorder;
+    public Transform TBorder;
+    public Transform BBorder;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("Zombie", 2, 5);
+	}
+	
+	// Update is called once per frame
+	void Zombie ()
+    {
+        int x = (int)Random.Range(LBorder.position.x, RBorder.position.x);
+
+        int y = (int)Random.Range(TBorder.position.y, BBorder.position.y);
+
+        Instantiate(Mob, new Vector2(x, y), Quaternion.identity);
+	}
+}
